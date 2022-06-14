9513772f42ab01314a909e6945aee8dc40b2e3ce
iskan.mustamir <iskan.mustamir@mail.ugm.ac.id>
2022-06-14 16:08:04 +0700

    feat(Add & Update): getLocation function

--- public/js/update.js
+++ public/js/update.js
@@ -1,47 +1,70 @@
-const storeForm = document.getElementById('store-form');
-const storeId = document.getElementById('store-id');
-const storeAddress = document.getElementById('store-address');
-const storeLat = document.getElementById('store-lat');
-const storeLong = document.getElementById('store-long');
+const storeForm = document.getElementById("store-form");
+const storeId = document.getElementById("store-id");
+const storeAddress = document.getElementById("store-address");
+const storeLat = document.getElementById("store-lat");
+const storeLong = document.getElementById("store-long");
+
+// Get user location automatically
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  storeLat.value = position.coords.latitude;
+  storeLong.value = position.coords.longitude;
+}
+
+function showError(error) {
+  alert(error.message);
+}
+getLocation();
 
 // Send POST to API to add store
 async function updateStore(e) {
   e.preventDefault();
 
-  if (storeId.value === '' || storeAddress.value === '' || storeLat.value === '' || storeLong.value === '') {
-    alert('Please fill in fields');
+  if (
+    storeId.value === "" ||
+    storeAddress.value === "" ||
+    storeLat.value === "" ||
+    storeLong.value === ""
+  ) {
+    alert("Please fill in fields");
   }
 
   const sendBody = {
     storeId: storeId.value,
     address: storeAddress.value,
     latitude: storeLat.value,
-    longitude: storeLong.value
-
+    longitude: storeLong.value,
   };
 
   try {
-    const res = await fetch('/api/v1/stores', {
-      method: 'PUT',
+    const res = await fetch("/api/v1/stores", {
+      method: "PUT",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(sendBody)
+      body: JSON.stringify(sendBody),
     });
 
     if (res.status === 400) {
-        throw Error('Store not exist!');
-      }
+      throw Error("Store not exist!");
+    }
     if (res.status === 401) {
-      throw Error('Store not exist!');
+      throw Error("Store not exist!");
     }
 
-    alert('Store added!');
-    window.location.href = '/index.html';
+    alert("Store added!");
+    window.location.href = "/index.html";
   } catch (err) {
     alert(err);
     return;
   }
 }
 
-storeForm.addEventListener('submit', updateStore);
+storeForm.addEventListener("submit", updateStore);
